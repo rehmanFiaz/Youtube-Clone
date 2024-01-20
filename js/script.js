a5ee812cdbbb17cc0365b089f6c93e0210c4d5ef
rehmanFiaz <rehmanfiazpc16666@gmail.com>
2024-01-20 18:32:53 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let menuIcon = document.querySelector('.menuIcon');
+let sideBar = document.querySelector('.sidebar');
+let container = document.querySelector('.container');
+
+function showMenu(){
+  sideBar.classList.toggle('small-sidebar');
+  container.classList.toggle('largeContainer');
+}
+
+
